9c2da741cea88d450fa8901ab8b455e021a98c7c
Antonioisrooted <antoniommaina@gmail.com>
2020-05-26 08:00:39 +0300

    new status

--- app/src/main/java/com/moringaschool/wakulima/FarmingActivity.java
+++ app/src/main/java/com/moringaschool/wakulima/FarmingActivity.java
@@ -1,6 +1,5 @@
 package com.moringaschool.wakulima;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,38 +10,37 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RestaurantsActivity extends AppCompatActivity {
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    public class FarmingActivity extends AppCompatActivity {
+        TextView mLocationTextView;
+        ListView mListView;
 
-    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
-    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+        private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
+        private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_farming);
-        ButterKnife.bind(this);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_farming);
+            ButterKnife.bind(this);
 
-        LearnFarmingArrayAdapter adapter = new LearnFarmingArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines); // must match constructor!
+            LearnFarmingArrayAdapter adapter = new LearnFarmingArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines); // must match constructor!
 
 //        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
+            mListView.setAdapter(adapter);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String restaurant = ((TextView)view).getText().toString();
+                    Toast.makeText(FarmingActivity.this, restaurant, Toast.LENGTH_LONG).show();
+                }
+            });
 
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
+            Intent intent = getIntent();
+            String location = intent.getStringExtra("location");
 
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+            mLocationTextView.setText("Here are all the restaurants near: " + location);
+        }
     }
-}
--- app/src/main/java/com/moringaschool/wakulima/MainActivity.java
+++ app/src/main/java/com/moringaschool/wakulima/MainActivity.java
@@ -1,14 +1,37 @@
 package com.moringaschool.wakulima;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mFindRestaurantsButton;
+    EditText mLocationEditText;
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, FarmingActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
