b8af90cc2a397c872dd3b8247ce35d351b14eae1
Antonioisrooted <antoniommaina@gmail.com>
2020-06-02 06:53:28 +0300

    added dependencies

---
+++ app/src/main/java/com/moringaschool/wakulima/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.wakulima;
+
+public class Constants {
+    public static final String GOV_BASE_URL = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?";
+    public static final String GOV_API_KEY = BuildConfig.GOV_API_KEY;
+}
